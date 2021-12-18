b2e32f0a294559d53064b181e660370c4f6d99e8
Denty <dentymods.axuka@gmail.com>
2021-12-18 18:27:27 -0500

    plugin base

---
+++ HighPingDetector/EventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Exiled.Events.EventArgs;
+using PlayerEvent = Exiled.Events.Handlers.Player;
+using ServerEvent = Exiled.Events.Handlers.Server;
+
+namespace HighPingDetector
+{
+    public class EventHandler
+    {
+    }
+}
--- HighPingDetector/HighPingDetector.cs
+++ HighPingDetector/HighPingDetector.cs
@@ -1,14 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Exiled.API.Features;
+using PlayerEvent = Exiled.Events.Handlers.Player;
+using ServerEvent = Exiled.Events.Handlers.Server;
 
 namespace HighPingDetector
 {
-    class HighPingDetector
+
+    public class HighPingDetector : Plugin<Config>
     {
 
+        private EventHandler EventHandler;
+        public static HighPingDetector Singleton;
 
+        public override string Name { get; } = "HighPingDetector";
+        public override string Author { get; } = "Denty";
+        public override string Prefix { get; } = "HighPingDetector";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(4, 0, 0);
+
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            EventHandler = new EventHandler();
+
+            Log.Info("HighPingDetector has been enabled.");
+            base.OnEnabled();
+        }
+
+
+        public override void OnDisabled()
+        {
+            Log.Info("HighPingDetector is disabled.");
+
+            EventHandler = null;
+            base.OnDisabled();
+        }
     }
 }
